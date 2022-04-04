99c324aa123a0f958bcd1f881856b8da04943f70
Ilya Volkov <illvolkov@bk.ru>
2022-04-05 02:52:34 +0500

    Create SettingsTableViewCell

---
+++ MVCSettingsTableViewLayout/Sources/Scenes/View/SettingsTableViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  SettingsTableViewCell.swift
+//  MVCSettingsTableViewLayout
+//
+//  Created by Ilya Volkov on 05.04.2022.
+//
+
+import Foundation
+import UIKit
+
+final class SettingsTableViewCell: UITableViewCell {
+    
+    static let identifier = "SettingsTableViewCell"
+    
+    private lazy var settingIcon: UIImageView = {
+        let icon = UIImageView()
+        return icon
+    }()
+    
+    private lazy var settingNameLabel: UILabel = {
+       let label = UILabel()
+       return label
+    }()
+    
+    private lazy var settingDetailedLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    private lazy var settingIndicatorBadge: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    private lazy var settingSwitch: UISwitch = {
+        let settingSwitch = UISwitch()
+        return settingSwitch
+    }()
+    
+}
