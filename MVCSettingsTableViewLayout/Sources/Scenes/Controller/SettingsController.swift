17ffcd9e862e2fd994dde67b5052f979b351138f
Ilya Volkov <illvolkov@bk.ru>
2022-04-08 04:30:06 +0500

    Create and setup navigation controller

--- MVCSettingsTableViewLayout/Sources/Scenes/Controller/SettingsController.swift
+++ MVCSettingsTableViewLayout/Sources/Scenes/Controller/SettingsController.swift
@@ -23,6 +23,37 @@ class SettingsController: UIViewController {
         model = SettingsModel()
         configureView()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        setupNavigationController()
+        navigationItem.hidesSearchBarWhenScrolling = false
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        navigationItem.hidesSearchBarWhenScrolling = true
+    }
+    
+    private func setupNavigationController() {
+
+        let searchView = UISearchController()
+        searchView.searchBar.tintColor = .systemBlue
+        searchView.searchBar.placeholder = "Поиск"
+        searchView.searchBar.setValue("Отменить", forKey: "cancelButtonText")
+
+        navigationItem.title = "Настройки"
+        navigationItem.searchController = searchView
+        navigationController?.navigationBar.prefersLargeTitles = true
+
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = .clear
+        appearance.shadowColor = .clear
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        navigationController?.navigationBar.standardAppearance = appearance
+    }
 
     private func configureView() {
         guard let models = model?.createModel() else { return }
